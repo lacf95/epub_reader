8698eccf59b6f3b399c84ba955689063627a60bd
Luis Adrián Chávez Fregoso <biolacf@gmail.com>
2025-07-12 09:28:37 -0600

    Refactors a little Cover extractor

--- lib/epub_reader/extractors/cover.rb
+++ lib/epub_reader/extractors/cover.rb
@@ -8,30 +8,41 @@ module EpubReader
       ns_entry :opf, "http://www.idpf.org/2007/opf"
 
       def self.extract(path)
-        item_href = open_opf(path) do |opf_doc, opf_doc_path, zip_file|
-          # Find the cover id otherwise use the default cover
-          cover_id = element_at(opf_doc, "//opf:metadata/opf:meta[@name='cover']")&.[]("content")
-          item_xpath = if cover_id
-            "//opf:manifest/opf:item[@id='#{cover_id}']"
-          else
-            "//opf:manifest/opf:item[contains(@href, 'cover') and contains(@media-type, 'image')]"
-          end
+        open_opf(path) do |opf_doc, opf_doc_path, zip_file|
+          cover_file_name = cover_reference(opf_doc)
+          return unless cover_file_name
 
-          item_href = element_at(opf_doc, item_xpath)&.[]("href")
-          return unless item_href
-
-          cover_file_path = File.join(File.dirname(opf_doc_path), item_href)
+          cover_file_path = File.join(File.dirname(opf_doc_path), cover_file_name)
           cover_file = zip_file.find_entry cover_file_path
           return unless cover_file
 
-          basename = File.basename cover_file_path
-          tempfile = Tempfile.new [basename]
-          tempfile.binmode
-          tempfile.write cover_file.get_input_stream.read
-          tempfile.rewind
-          ::EpubReader::Cover.new(name: basename, file: tempfile)
+          cover_basename = File.basename cover_file_path
+          tempfile = copy_to_temp_file(cover_file, cover_basename)
+          ::EpubReader::Cover.new(name: cover_basename, file: tempfile)
         end
       end
+
+      def self.cover_reference(opf_doc)
+        cover_id = element_at(opf_doc, "//opf:metadata/opf:meta[@name='cover']")&.[]("content")
+        item_xpath = if cover_id
+          "//opf:manifest/opf:item[@id='#{cover_id}']"
+        else
+          "//opf:manifest/opf:item[contains(@href, 'cover') and contains(@media-type, 'image')]"
+        end
+
+        element_at(opf_doc, item_xpath)&.[]("href")
+      end
+
+      def self.copy_to_temp_file(file, file_name)
+        tempfile = Tempfile.new [file_name]
+        tempfile.binmode
+        tempfile.write file.get_input_stream.read
+        tempfile.rewind
+        tempfile
+      end
+
+      private_class_method :cover_reference
+      private_class_method :copy_to_temp_file
     end
   end
 end
